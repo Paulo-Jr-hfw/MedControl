a83a386bfa7d509752f8d00b6aa8d1e2ba9f7af5
Paulo Junior <paulojunior.hfw@gmail.com>
2026-03-04 15:11:28 +0000

    feat: Criação do sistema de rotas utilizando Sealed Class para maior segurança.

---
+++ app/src/main/java/com/app/medcontrol/navigation/Routes.kt
@@ -0,0 +1,9 @@
+package com.app.medcontrol.navigation
+
+sealed class Routes (val route: String) {
+    object Login: Routes("login")
+    object CadastroUser: Routes("cadastro_user")
+    object HomeScreen: Routes("home_screen")
+
+
+}
