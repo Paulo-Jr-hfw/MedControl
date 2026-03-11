ef4bf9d60a79a139bbd2f758b555478fe4d25b62
Paulo Junior <paulojunior.hfw@gmail.com>
2026-03-11 12:57:11 +0000

    feat: Mudança do Banco de Dados para vínculo de medicamento com usuarioId

--- app/src/main/java/com/app/medcontrol/data/AppDataBase.kt
+++ app/src/main/java/com/app/medcontrol/data/AppDataBase.kt
@@ -6,7 +6,7 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
-@Database(entities = [MedicamentoEntity::class, UsuarioEntity::class], version = 2, exportSchema = false)
+@Database(entities = [MedicamentoEntity::class, UsuarioEntity::class], version = 1, exportSchema = false)
 @TypeConverters(Converters::class)
 abstract class AppDataBase: RoomDatabase() {
     abstract fun medicamentoDao(): MedicamentoDao
