421868550228564b45c23ed1d07ffbb69b2daa4c
Paulo Junior <paulojunior.hfw@gmail.com>
2026-03-20 15:29:56 +0000

    refactor: implementa soft-delete de medicamentos e sincronização inteligente

---
+++ app/src/main/java/com/app/medcontrol/screen/medicamento/MedicamentoScreen.kt
@@ -0,0 +1,11 @@
+package com.app.medcontrol.screen.medicamento
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MedicamentoScreen() {
+
+    //HeaderMedicamento()
+    //ListaMedicamentos()
+
+}
