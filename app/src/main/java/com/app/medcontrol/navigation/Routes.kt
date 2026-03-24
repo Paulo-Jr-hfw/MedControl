8f8d3c50e99dd5fde0987123575073f733808d7a
Paulo Junior <paulojunior.hfw@gmail.com>
2026-03-24 15:51:11 +0000

    feat: integra usuarioId ao fluxo de navegação interna

--- app/src/main/java/com/app/medcontrol/navigation/Routes.kt
+++ app/src/main/java/com/app/medcontrol/navigation/Routes.kt
@@ -1,11 +1,14 @@
 package com.app.medcontrol.navigation
 
-sealed class Routes (val route: String) {
+sealed class Routes(val route: String) {
     object Login: Routes("login")
     object CadastroUser: Routes("cadastro_user")
+
     object HomeScreen: Routes("home_screen")
 
     object CadastroMed: Routes("cadastro_med")
-
+    object Medicamentos : Routes("medicamentos")
+    object Sinais : Routes("sinais")
+    object Historico : Routes("historico")
 
 }
