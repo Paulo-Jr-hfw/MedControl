99cbc5c0a2828e827a390b9b93d69053c8d5506f
Paulo Junior <paulojunior.hfw@gmail.com>
2026-05-18 16:35:26 +0100

    fix: impede que medicamentos futuros entrem como atrasados antes da hora

--- app/src/main/java/com/app/medcontrol/components/MedicamentoItem.kt
+++ app/src/main/java/com/app/medcontrol/components/MedicamentoItem.kt
@@ -1,7 +1,5 @@
 package com.app.medcontrol.components
 
-import android.os.Build
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
