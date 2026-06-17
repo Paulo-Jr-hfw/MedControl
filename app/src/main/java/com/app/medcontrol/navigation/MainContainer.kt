7ece3b0195a28b69c0c9ed407ba7bc2b487c373b
Paulo Junior <paulojunior.hfw@gmail.com>
2026-06-17 15:22:03 +0100

    UI: refatoração visual e ajustes de padding

--- app/src/main/java/com/app/medcontrol/navigation/MainContainer.kt
+++ app/src/main/java/com/app/medcontrol/navigation/MainContainer.kt
@@ -2,8 +2,6 @@ package com.app.medcontrol.navigation
 
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
