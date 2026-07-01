54295f394ad4414f7bb2e5225350040f47b17f12
Paulo Junior <paulojunior.hfw@gmail.com>
2026-07-01 16:45:12 +0100

    refactor: adicionado campo para retirar imagem do banco quando o medicamento for excluido tambem.

--- app/src/main/java/com/app/medcontrol/domain/usecase/ExcluirMedicamentoUseCase.kt
+++ app/src/main/java/com/app/medcontrol/domain/usecase/ExcluirMedicamentoUseCase.kt
@@ -1,16 +1,19 @@
 package com.app.medcontrol.domain.usecase
 
-import com.app.medcontrol.data.entity.MedicamentoEntity
 import com.app.medcontrol.repository.MedicamentoRepository
 import com.app.medcontrol.service.AlarmScheduler
+import com.app.medcontrol.util.ImageStorageManager
 import javax.inject.Inject
 
 class ExcluirMedicamentoUseCase @Inject constructor(
     private val repository: MedicamentoRepository,
-    private val alarmScheduler: AlarmScheduler
+    private val alarmScheduler: AlarmScheduler,
+    private val imageStorageManager: ImageStorageManager
 ) {
     suspend operator fun invoke(medicamentoId: Int) {
         val medicamento = repository.getMedicamentoById(medicamentoId) ?: return
+
+        imageStorageManager.deleteImage(medicamento.imagemUri)
 
         val mId = medicamento.id
         val medInativoSemFoto = medicamento.copy(ativo = false, imagemUri = null)
